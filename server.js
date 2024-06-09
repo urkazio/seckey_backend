6473e77df26a98e63cf7d53ded3ccfc7f38e39a9
urkazio <urkoupv@gmai.com>
2024-06-09 14:09:23 +0200

    comentario

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const https = require('https');
 const app = require('./app');
 
-// Leer los archivos del certificado
+// Leer los archivos del certificado SSL
 const options = {
     key: fs.readFileSync('D:/UNIVERSIDAD/master/TFM/SERVIDOR/server.key'),
     cert: fs.readFileSync('D:/UNIVERSIDAD/master/TFM/SERVIDOR/server.cert')
